0d5dcb9186195a0e2bd93df60f3797adf343576f
daryapatsko <darya.patsko@mail.ru>
2023-04-17 15:40:22 +0300

    added slider at customers section

--- Axit/src/index.js
+++ Axit/src/index.js
@@ -1,6 +1,15 @@
+new Swiper('.swiper-container',{
+    spaceBetween:30,
+    pagination:{
+        el:'.swiper-pagination',
+        clickable:true,
+    },
+});
+
 document.addEventListener("DOMContentLoaded", function(){
     document.getElementById("burger").addEventListener("click",function(){
         document.querySelector(".header").classList.toggle("open"),
         document.querySelector(".menu_header").classList.toggle("open")
     })
-})
+});
+
